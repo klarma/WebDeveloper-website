c8d15a19a230dddd505798312425f2605410388b
Katarzyna Napierała <k_napierala@op.pl>
2020-11-07 16:53:14 +0100

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,24 @@
+const $doc = $(document);
+const $header = $('header');
+
+
+$('header button').on('click', function () {
+    $('body ,html').animate({
+        scrollTop: $('.portfolio').offset().top
+    }, 800)
+})
+
+$doc.on('scroll', function () {
+    const scrollSize = $doc.scrollTop();
+    const headerHeight = $header.outerHeight();
+
+    $header.css({
+        'opacity': 1 - scrollSize / headerHeight,
+        'filter': 'grayscale(' + 2 * scrollSize / headerHeight + ')'
+    })
+    // $('header>*').css('transform', 'scale(0)');
+})
+
+
+
+// przerzuć na poczatek
